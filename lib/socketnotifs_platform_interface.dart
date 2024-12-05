4872769996f933a2df0f2f9f6102c13bc0f273ce
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-12-05 20:50:11 +0530

    modified:   LICENSE 	modified:   lib/socketnotifs_platform_interface.dart

--- lib/socketnotifs_platform_interface.dart
+++ lib/socketnotifs_platform_interface.dart
@@ -2,19 +2,15 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:socketnotifs/socketnotifs.dart';
 
 abstract class SocketNotifsPlatform extends PlatformInterface {
-  // Constructor
   SocketNotifsPlatform() : super(token: _token);
 
   static final Object _token = Object();
 
-  // This method must be implemented by the native code
   Future<void> connectToWebSocket(String url);
 
-  // A getter to access the current platform-specific implementation
   static SocketNotifsPlatform get instance => _instance;
   static SocketNotifsPlatform _instance = SocketNotifsMethodChannel();
 
-  // Sets the platform implementation (useful for testing or custom platforms)
   static set instance(SocketNotifsPlatform instance) {
     _instance = instance;
   }
