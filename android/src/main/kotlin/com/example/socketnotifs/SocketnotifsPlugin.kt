fa2cfeccf3f953f84547dcdbc4d6bfa0aae34df8
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-12-07 00:41:41 +0530

    modified:   android/src/main/kotlin/com/example/socketnotifs/SocketnotifsPlugin.kt

--- android/src/main/kotlin/com/example/socketnotifs/SocketnotifsPlugin.kt
+++ android/src/main/kotlin/com/example/socketnotifs/SocketnotifsPlugin.kt
@@ -1,7 +1,9 @@
 package com.example.socketnotifs
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Log
 import androidx.core.content.ContextCompat
@@ -30,17 +32,26 @@ class SocketnotifsPlugin: FlutterPlugin, MethodCallHandler {
       val url = call.argument<String>("url")
 
       if (url != null) {
-        val intent = Intent(context, WebSocketService::class.java).apply {
-          putExtra("webSocketUrl", url) // Pass the URL to the WebSocketService
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-          ContextCompat.startForegroundService(context, intent)
+        // Check if notification permission is granted
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { // Android 13 and above
+          if (ContextCompat.checkSelfPermission(
+              context,
+              Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+          ) {
+            startNotificationService(url, result)
+          } else {
+            Log.e("WebSocketService", "Notification permission not granted")
+            result.error(
+              "PERMISSION_DENIED",
+              "Notification permission not granted",
+              null
+            )
+          }
         } else {
-          context.startService(intent)
+          // For Android versions below 13, start the service directly
+          startNotificationService(url, result)
         }
-
-        result.success("Notification service started successfully")
       } else {
         Log.e("WebSocketService", "URL is null")
         result.error("INVALID_ARGUMENT", "WebSocket URL is null", null)
@@ -48,6 +59,20 @@ class SocketnotifsPlugin: FlutterPlugin, MethodCallHandler {
     } else {
       result.notImplemented()
     }
+  }
+
+  private fun startNotificationService(url: String, result: Result) {
+    val intent = Intent(context, WebSocketService::class.java).apply {
+      putExtra("webSocketUrl", url) // Pass the URL to the WebSocketService
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      ContextCompat.startForegroundService(context, intent)
+    } else {
+      context.startService(intent)
+    }
+
+    result.success("Notification service started successfully")
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
